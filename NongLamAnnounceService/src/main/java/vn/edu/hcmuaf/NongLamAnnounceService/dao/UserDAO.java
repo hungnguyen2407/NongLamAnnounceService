30ee6a05768496bf01b10a6292804b14101d1fc3
Hung Nguyen <14130047@st.hcmuaf.edu.vn>
2018-04-16 21:42:47 +0700

    UserDAO

--- NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/UserDAO.java
+++ NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/UserDAO.java
@@ -1,11 +1,18 @@
 package vn.edu.hcmuaf.NongLamAnnounceService.dao;
 
+import java.sql.Date;
+import java.sql.ResultSet;
+
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.PreparedStatement;
+
 public class UserDAO {
 
 	public static boolean login(String id, String pass)
 	{
 		boolean valid = false;
-		
+		Connection conn = MyConnection.getConnection();
+		//TODO
 		return valid;
 	}
 }
