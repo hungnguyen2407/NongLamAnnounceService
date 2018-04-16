0c46cfe143c2934b4aba39e7e5a6c15abfea9f39
Hung Nguyen <14130047@st.hcmuaf.edu.vn>
2018-04-16 12:09:58 +0700

    Connection

--- NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/Connection.java
+++ NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/Connection.java
@@ -1,5 +1,31 @@
 package vn.edu.hcmuaf.NongLamAnnounceService.dao;
 
-public class Connection {
+import java.sql.DriverManager;
 
+public class Connection {
+	public static Connection getConnection() {
+		Connection connection = null;
+		String driver = "com.mysql.jdbc.Driver";
+		String url = "jdbc:mysql://localhost:3306/NotiApp";
+		String user = "root";
+		String pass = "admin";
+		try {
+			Class.forName(driver);
+			connection = (Connection) DriverManager.getConnection(url, user, pass);
+			
+		} catch (Exception e) {
+			// Orther err
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	public static void main(String[] args) {
+		Connection connection = getConnection();
+		if (connection != null) {
+			System.out.println("Kết nối thành công");
+		} else {
+			System.out.println("Kết nối thất bại");
+		}
+	}
 }
