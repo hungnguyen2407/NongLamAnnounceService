3a86d49408b4cb969b2504fb67ead3e9bd10b5c7
nguyen tu <nguyen tu@192.168.1.34>
2018-04-25 14:31:20 +0700

    Thêm phương thức thoát nhóm và kiểm duyệt xin vào nhóm

--- NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/GroupDAO.java
+++ NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/dao/GroupDAO.java
@@ -23,4 +23,64 @@ public class GroupDAO {
 		}
 		return false;
 	}
+	
+	public static boolean outGroup(String class_id, String id){
+		int i;
+		try {
+			Connection conn = MyConnection.getConnection();
+			String sql = "call p_del_list(?, ?);";
+			PreparedStatement pr = (PreparedStatement) conn.prepareStatement(sql);
+			pr.setString(1, class_id);
+			pr.setString(2, id);
+			i = pr.executeUpdate();
+			pr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		if (i > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean accedeGroup(String class_id, String id) {
+		int i;
+		try {
+			Connection conn = MyConnection.getConnection();
+			String sql = "insert into temp_group values (?, ?);";
+			PreparedStatement pr = (PreparedStatement) conn.prepareStatement(sql);
+			pr.setString(1, class_id);
+			pr.setString(2, id);
+			i = pr.executeUpdate();
+			pr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		if (i > 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public static boolean outAccedeGroup(String class_id, String id) {
+		int i;
+		try {
+			Connection conn = MyConnection.getConnection();
+			String sql = "delete from temp_group where class_id = ? and id = ?;";
+			PreparedStatement pr = (PreparedStatement) conn.prepareStatement(sql);
+			pr.setString(1, class_id);
+			pr.setString(2, id);
+			i = pr.executeUpdate();
+			pr.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		if (i > 0) {
+			return true;
+		}
+		return false;
+	}
 }
--- NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/services/GroupServices.java
+++ NongLamAnnounceService/src/main/java/vn/edu/hcmuaf/NongLamAnnounceService/services/GroupServices.java
@@ -1,20 +1,81 @@
 package vn.edu.hcmuaf.NongLamAnnounceService.services;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-
 
 import vn.edu.hcmuaf.NongLamAnnounceService.dao.GroupDAO;
 
 @Path("/group")
 public class GroupServices {
 
+	/**
+	 * Phương thức thêm thành viên vào group hoặc lớp
+	 * 
+	 * @param class_id
+	 *            mã group hoặc lớp
+	 * @param id
+	 *            mã user
+	 * @return "true" hoặc "false"
+	 */
 	@POST
 	@Path("/join/{class_id}/{id}")
 	@Produces("application/json; charset=UTF-8")
-	public String joinGroup(@PathParam("class_id")String class_id, @PathParam("id")String id) {
+	public String joinGroup(@PathParam("class_id") String class_id, @PathParam("id") String id) {
 		return GroupDAO.joinGroup(class_id, id) ? "true" : "false";
+	}
+
+	/**
+	 * Phương thức rời khỏi group hoặc lớp
+	 * 
+	 * @param class_id
+	 *            mã group hoặc lớp
+	 * @param id
+	 *            mã user
+	 * @return "true" hoặc "false"
+	 */
+	@DELETE
+	@Path("/out/{class_id}/{id}")
+	@Produces("application/json; charset=UTF-8")
+	public String outGroup(@PathParam("class_id") String class_id, @PathParam("id") String id) {
+		return GroupDAO.outGroup(class_id, id) ? "true" : "false";
+	}
+
+	/**
+	 * Phương thức xin gia nhập group hoặc lớp
+	 * 
+	 * @param class_id
+	 *            mã group hoặc lớp
+	 * @param id
+	 *            mã user
+	 * @return "true" hoặc "false"
+	 */
+	@POST
+	@Path("/join/{class_id}/{id}")
+	@Produces("application/json; charset=UTF-8")
+	public String accedeGroup(@PathParam("class_id") String class_id, @PathParam("id") String id) {
+		return GroupDAO.accedeGroup(class_id, id) ? "true" : "false";
+	}
+
+	/**
+	 * Phương thức cho phép gia nhập group hoặc lớp
+	 * 
+	 * @param class_id
+	 *            mã group hoặc lớp
+	 * @param id
+	 *            mã user
+	 * @return "true" hoặc "false"
+	 */
+	@POST
+	@Path("/approved/{class_id}/{id}")
+	@Produces("application/json; charset=UTF-8")
+	public String approvedGroup(@PathParam("class_id") String class_id, @PathParam("id") String id) {
+		if (GroupDAO.outAccedeGroup(class_id, id)) {
+			return GroupDAO.joinGroup(class_id, id) ? "true" : "false";
+		} else {
+			return "false";
+		}
 	}
 }
